91704553e3a2d7e03e8c82dd0f0796c44257a079
Xiang Li <lxmichael52@gmail.com>
2022-05-10 16:22:58 -0700

    spinner1.js added

---
+++ spinner1.js
@@ -0,0 +1,16 @@
+setTimeout(() => {
+  process.stdout.write('\r|   ');
+}, 100);
+
+setTimeout(() => {
+  process.stdout.write('\r/   ');
+}, 300);
+
+setTimeout(() => {
+  process.stdout.write('\r-   ');
+}, 500);
+
+setTimeout(() => {
+  // Need to escape the backslash since it's a special character.
+  process.stdout.write('\r\\   '); 
+}, 700);
